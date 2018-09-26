ca36d3e2291bdb527279eee3135694645f4a36fc
jinlong.zang <502242791@qq.com>
2018-09-26 13:12:40 +0800

    order

--- pages/page/update/update.js
+++ pages/page/update/update.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    value:'',
+    value: '',
   },
 
   /**
@@ -14,7 +14,8 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
-      value:options.name
+      value: options.name,
+      type: options.type
     })
   },
   // 清空input的内容
@@ -23,75 +24,94 @@ Page({
       value: ''
     })
   },
-  searchInput(e){
+  searchInput(e) {
     let val = e.detail.value;
     this.setData({
       value: val
     })
   },
-  save(){
-    let text = this.data.value;
-    if(text){
-      app.http.putRequest("/api/user/nickname/" + text).then(res => {
-        if (res.success) {
-          wx.navigateBack({
-            
-          })
-        }
-      })
-    }else{
+  save() {
+    let text = this.data.value.trim();
+    let type = this.data.type;
+    let url = "";
+    let obj = {};
+    if (text == '') {
       wx.showToast({
-        title: '请输入昵称',
+        title: '请填写',
         icon: "none"
       })
+      return
+    }
+
+    if (type == 'name') {
+      url = "/api/user/nickname/{{nickName}}";
+      obj = { nickName: text };
+    } else if (type == 'wx') {
+      url = "/api/user/weixinNumber/{{weixinNumber}}";
+      obj = { weixinNumber: text };
+    }
+
+    if (text) {
+      app.http.putRequest(url, obj).then(res => {
+        this.after(res);
+      })
+    }
+  },
+  after(res) {
+    wx.showToast({
+      title: res.message,
+      icon: 'none'
+    })
+    if (res.success) {
+      wx.navigateBack({})
     }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
