6c065a473e51772954a8fc0967686e70dbb6b8c1
jinlong.zang <502242791@qq.com>
2018-11-30 21:05:58 +0800

    a

--- pages/page/workIndex/workIndex.js
+++ pages/page/workIndex/workIndex.js
@@ -81,6 +81,20 @@ Page({
      })
    })
  },
+  getStore() {
+    Api.storeIdInfo().then(res => {
+      let store = res.obj.store[0].store;
+      if (!store.name) {
+        this.setData({
+          initOrder: true
+        })
+      } else {
+        this.setData({
+          initOrder: false
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -92,7 +106,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getStore();
   },
 
   /**
