57227bd86b00ef9c38ba4900f78bcde05a6ed30e
zjl <502242791@qq.com>
2019-03-05 14:13:42 +0800

    goodslist

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ App({
       wx.setStorageSync("storeId",options.query.storeId)
     }
     // wx.setStorageSync("storeId", "S1000631")
-    // wx.setStorageSync("storeId", "S1000349")
+    wx.setStorageSync("storeId", "S1000349")
     // wx.setStorageSync("storeId", "S001")
     // wx.setStorageSync("storeId", "S1000631")
     // 获取小程序更新机制兼容
