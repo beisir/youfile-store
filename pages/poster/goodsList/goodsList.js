f4b2809462c6cdee26e21fb628651c3ebaf4d7d2
zjl <502242791@qq.com>
2019-03-06 11:54:55 +0800

    chosegoods

--- pages/poster/goodsList/goodsList.js
+++ pages/poster/goodsList/goodsList.js
@@ -5,62 +5,123 @@ Page({
    * 页面的初始数据
    */
   data: {
+    goods: [{
+        name: '周大福 赵丽颖赵丽颖陪伴款颖火虫赵丽颖陪伴款颖火虫陪伴款颖火虫',
+        checked: false,
+        img: [{
+          checked: false
+        }, {
+          checked: false
+        }, {
+          checked: false
+        }, {
+          checked: false
+        }, {
+          checked: false
+        }, {
+          checked: false
+        }],
+        id: 1
+      },
+      {
+        name: '周大福 赵丽颖赵丽颖陪伴款颖火虫赵丽颖陪伴款颖火虫陪伴款颖火虫',
+        checked: false,
+        img: [{
+          checked: false
+        }, {
+          checked: false
+        }, {
+          checked: false
+        }],
+        id: 2
+      }
+    ]
+  },
+  checkedGoods(e) {
+    let arr = this.data.goods,
+      thisItem = e.currentTarget.dataset.item;
+    arr.forEach(el => {
+      if (el.id === thisItem.id) {
+        el.checked = true
+        el.img[0].checked = true
+        this.setData({
+          nowChecked: el
+        })
+      } else {
+        el.checked = false
+        el.img.forEach(ii => {
+          ii.checked = false
+        })
+      }
+    })
+
+    this.setData({
+      goods: arr
+    })
+  },
+  checkedImg(e) {
+    let goodsindex = e.currentTarget.dataset.goodsindex,
+      imgindex = e.currentTarget.dataset.index,
+      item = e.currentTarget.dataset.item;
+    this.setData({
+      ['goods[' + goodsindex + '].img[' + imgindex + '].checked']: !item.checked
+    })
+
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
