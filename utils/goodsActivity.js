140c1b4507506ac4842fc2a43703331c93a6409c
zjl <502242791@qq.com>
2019-04-09 20:49:35 +0800

    address

---
+++ utils/goodsActivity.js
@@ -0,0 +1,48 @@
+// 把活动绑定sku上
+function bindingTOSKU(goodsVO, entry) {
+  switch(entry){
+    case 'preorder':
+      var goodsSkuVOList = goodsVO.preOrderGoodsSkuList
+      var acInfo = goodsVO.promotions.SALES_PROMOTION
+    break;
+    default:
+    break;
+  }
+  // 判断是否有额外参数
+  if (acInfo && acInfo.length > 0) {
+    goodsVO.hasActiveGoods = true
+    var standardGoodsSkuPromotions = acInfo[0].standardGoodsSkuPromotions
+    if (standardGoodsSkuPromotions){
+      for (var val of goodsSkuVOList) {
+        val.standardGoodsSkuPromotions = []
+        for (var v of standardGoodsSkuPromotions) {
+          if (val.skuCode == v.skuCode) {
+            val.standardGoodsSkuPromotions.push(v)
+            val.isActivity = true
+            val.saleBatch = 5
+            val.activityPrice = v.activityPrice
+            goodsVO.hasActiveGoods = true
+          }
+        }
+      }
+    } else {
+      goodsVO.noSkuActiveGoods = true
+      goodsVO.activityPrice = acInfo[0].activityPrice
+    }
+  } else {
+    goodsVO.hasActiveGoods = false
+  }
+  return goodsVO
+}
+
+function goodsListBindingSku(goodsList, entry){
+  goodsList.forEach(el=>{
+    el = bindingTOSKU(el, entry)
+  })
+  return goodsList
+}
+
+module.exports = {
+  goodsListBindingSku,
+  bindingTOSKU
+}
