e2f09561f0888d6a979c2cd46b6b357b3b9f2ae9
jinlong.zang <502242791@qq.com>
2019-07-01 19:21:21 +0800

    warehouse

---
+++ distribution/pages/warehouse/warehouseDetail/warehouseDetail.js
@@ -0,0 +1,127 @@
+// distribution/pages/warehouse/warehouseDetail/warehouseDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    formData: {
+      warningGoods: false,
+      filterZero: false
+    },
+    classList: [{
+      name: '男装'
+    }, {
+      name: '女装'
+    }, {
+      name: '女装'
+    }, {
+      name: '女装'
+    }, {
+      name: '女装'
+    }, {
+      name: '女装'
+    }],
+    warehouseList: [{
+      name:'123',
+    }]
+  },
+  showFilter() {
+    const side = this.selectComponent('#side')
+    side.show()
+  },
+  selectClass(e) {
+    let thisindex = e.currentTarget.dataset.index
+    this.setData({
+      ["classList[" + thisindex + "].selected"]: !this.data.classList[thisindex].selected
+    })
+  },
+  switchChange(e) {
+    let type = e.currentTarget.dataset.type,
+      obj = {}
+    switch (type) {
+      case "warning":
+        obj = {
+          ["formData.warningGoods"]: e.detail.value
+        }
+        break;
+      case "zero":
+        obj = {
+          ["formData.filterZero"]: e.detail.value
+        }
+        break;
+    }
+    this.setData(obj)
+  },
+  sureForm() {
+    let arr = this.data.classList.filter(el => el.selected)
+  },
+  resetForm() {
+    let arr = this.data.classList
+    arr.forEach(el => {
+      el.selected = false
+    })
+    this.setData({
+      formData: {
+        warningGoods: false,
+        filterZero: false,
+        classList: arr
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
