7f14c9b02b9835b5a1943dbc8be7948bfa94f840
jinlong.zang <502242791@qq.com>
2019-10-21 18:35:21 +0800

    default store

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ App({
     // wx.setStorageSync("storeId", "S1000631")
     // wx.setStorageSync("storeId", "S1000349")
     // wx.setStorageSync("storeId", "S001")
-    // wx.setStorageSync("storeId", "S1000323")
+    wx.setStorageSync("storeId", "S1000183")  // 默认店铺
     // 获取小程序更新机制兼容
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
